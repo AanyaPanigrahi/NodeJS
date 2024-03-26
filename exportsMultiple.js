6b4f33e1ccb1c00740d410aa6e0a5963a09d2bdb
Aanya Panigrahi <114793986+AanyaPanigrahi@users.noreply.github.com>
2024-03-27 01:17:28 +0530

    Add files via upload

---
+++ exportsMultiple.js
@@ -0,0 +1,13 @@
+function myFunction1() {
+    console.log('Hello from myFunction1!');
+  }
+  
+  function myFunction2() {
+    console.log('Hello from myFunction2!');
+  }
+  
+  module.exports = {
+    str: 'hey',
+    myFunc1: myFunction1,
+    myFunc2: myFunction2
+  };
---
+++ hello.js
@@ -0,0 +1,4 @@
+function sayHi(name){
+    console.log(`hello ${name}`);
+}
+module.exports=sayHi
